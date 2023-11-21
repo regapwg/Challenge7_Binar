007a9d36e69e6b7131f72e411ae69a497e632a1e
regapwg <regapwg@gmail.com>
2023-11-21 20:10:25 +0800

    add unit testing api

---
+++ app/src/test/java/com/example/challenge2_binar/database/cartDb/SimpleDatabaseTest.kt
@@ -0,0 +1,31 @@
+package com.example.challenge2_binar.database.cartDb
+
+//@RunWith(AndroidJUnit4::class)
+//@SmallTest
+class SimpleDatabaseTest {
+//
+//    private lateinit var db : SimpleDatabase
+//    private lateinit var dao : SimpleChartDao
+//
+//    @Before
+//    fun setUpDatabase(){
+//        val context =  ApplicationProvider.getApplicationContext<Context>()
+//        db = Room.inMemoryDatabaseBuilder(context,SimpleDatabase::class.java).allowMainThreadQueries().build()
+//        dao = db.simpleChartDao
+//    }
+//
+//    @After
+//    fun closeDb(){
+//        db.close()
+//    }
+//
+//   @Test
+//    fun insertTest() {
+//        val product = SimpleChart(1,"chittos",1,"url",20000, 40000)
+//        dao.insert(product)
+//
+//        val products = dao.getAllCartItems()
+//        assertNotNull(products)
+//
+//    }
+}
